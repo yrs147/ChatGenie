b1d167a29fad167ab4fea12640538c72ff907b26
yrs147 <yashraj14700728@gmail.com>
2023-01-21 17:03:51 +0530

    Added Go-GPT3 library function to call the API

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/PullRequestInc/go-gpt3"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+func GetResponse(client gpt3.Client , ctx context.Context , question string){
+	client.CompletionStreamWithEngine(ctx , gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt: []string{
+			question,
+		},
+		MaxTokens: gpt3.IntPtr(3000),
+		Temperature:  gpt3.Float32Ptr(0),
+		},func(resp *gpt3.CompletionResponse){
+			fmt.Print(resp.Choices[0].Text)
+		})
+		if err != nil{
+			fmt.Println(err)
+			os.Exit(13)
+		}
+
+	}
+}
+
 func main() {
 	viper.SetConfigFile(".env")
-	viper.ReadinConfig()
+	viper.ReadInConfig()
 	apiKey := viper.GetString("API_KEY")
 	if apiKey == "" {
 		panic("API KEY Missing")
@@ -18,16 +40,27 @@ func main() {
 	ctx := context.Background()
 	client := gpt3.NewClient(apiKey)
 	rootCmd := &cobra.Command{
-		Use: "chatgpt",
+		Use:   "chatgpt",
 		Short: "Chat with ChatGPT in console.",
-		Run: func(cmd *cobra.Command , args []string){
+		Run: func(cmd *cobra.Command, args []string) {
 			scanner := bufio.NewScanner(os.Stdin)
 			quit := false
 
-			for !quit{
+			for !quit {
 				fmt.Print("Say Something('quit' to end):")
-			}
-		}
-	}
+				if !scanner.Scan() {
+					break
+				}
+				question := scanner.Text()
+				switch question {
+				case "quit":
+					quit = true
 
+				default:
+					GetResponse(client, ctx, question)
+				}
+			}
+		},
+	}
+	rootCmd.Execute()
 }
